29c36352b83969b073bc431faae1c4f1ba35cee3
Franco <rochafrancoagustin@hotmail.com>
2024-02-28 15:44:28 -0300

    Se agrego controlador Ping

---
+++ ServidorParaBot/Controllers/PingController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServidorParaBot.Controllers
+{
+    [ApiController]
+    [Route("Ping")]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Pong";
+        }
+    }
+}
