91b10cad1673b25126a9dbb2675bf9b70545c44e
Franco <rochafrancoagustin@hotmail.com>
2024-03-01 16:17:52 -0300

    se termino de configurar la autenticacion

--- ServidorParaBot/Controllers/InteractionsController.cs
+++ ServidorParaBot/Controllers/InteractionsController.cs
@@ -1,5 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto.Signers;
+using Org.BouncyCastle.Utilities.Encoders;
 using System.Net;
+using System.Security;
+using System.Text;
 
 namespace ServidorParaBot.Controllers
 {
@@ -8,18 +13,43 @@ namespace ServidorParaBot.Controllers
     public class InteractionsController : ControllerBase
     {
         [HttpPost]
-        public ActionResult Post([FromHeader(Name = "X-Signature-Ed25519")] string? signature, [FromHeader(Name = "X-Signature-Timestamp")] string? timestamp)
+        public async Task<ActionResult> Post([FromHeader(Name = "X-Signature-Ed25519")] string? signature, [FromHeader(Name = "X-Signature-Timestamp")] string? timestamp)
         {
+
             if (String.IsNullOrWhiteSpace(signature) || String.IsNullOrWhiteSpace(timestamp))
             {
                 return Unauthorized();
             }
 
-            return Ok(new Interactions(1));
+            string body = string.Empty;
+            using (var reader = new StreamReader(Request.Body,
+                          encoding: Encoding.UTF8, detectEncodingFromByteOrderMarks: false))
+            {
+                body = await reader.ReadToEndAsync();
+
+                if (VerifySignature(Environment.GetEnvironmentVariable("PUBLIC_KEY"), timestamp + body, signature))
+                {
+                    return Ok(new Interactions(1));
+                }
+            }
+
+            return Unauthorized();    
 
         }
 
-        
+        public bool VerifySignature(string publicKey, string dataToVerify, string signature)
+        {
+            var publicKeyParam = new Ed25519PublicKeyParameters(Hex.DecodeStrict(publicKey));
+            var dataToVerifyBytes = Encoding.UTF8.GetBytes(dataToVerify);
+            var signatureBytes = Convert.FromHexString(signature);
+
+            var verifier = new Ed25519Signer();
+            verifier.Init(false, publicKeyParam);
+            verifier.BlockUpdate(dataToVerifyBytes, 0, dataToVerifyBytes.Length);
+            var isVerified = verifier.VerifySignature(signatureBytes);
+            return isVerified;
+        }
+
 
     }
 
